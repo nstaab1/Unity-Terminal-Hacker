533a82d88f827e58132e3d085f28d299361a2e44
Nathan Staab <nstaab443@gmail.com>
2019-07-02 19:55:44 -0400

    Working prototype of game

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -8,6 +8,12 @@ public class Hacker : MonoBehaviour
     //Game State
     int level;
     enum Screen { MainMenu, Password, Win};
+    enum Level { NewGame, Programming, Hockey, Space}
+    string[] programmingPasswords = { "array", "object", "binary", "java", "variable" };
+    string[] hockeyPasswords = { "stick", "puck", "goal", "slapshot", "check" };
+    string[] spacePasswords = { "nebula", "spaceship", "blackhole", "asteroid", "gravity" };
+    string password;
+    Level currentLevel;
     Screen currentScreen;
 
     // Start is called before the first frame update
@@ -25,8 +31,7 @@ public class Hacker : MonoBehaviour
     void OnUserInput(string input)
     {
         if (input.ToLower() == "menu")
-        {
-            
+        { 
             Terminal.ClearScreen();
             ShowMainMenu();
         }
@@ -34,21 +39,59 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
 
+    }
+
+    private void CheckPassword(string input)
+    {
+        if (input.ToLower() == password)
+        {
+            currentScreen = Screen.Win;
+            RunWinScreen();
+        }
+        else
+        {
+            Terminal.WriteLine("Incorrect Password. Try Again.");
+            StartGame();
+        }
+    }
+
+    private void RunWinScreen()
+    {
+        password = "";
+        currentLevel = Level.NewGame;
+        Terminal.WriteLine("");
+        Terminal.WriteLine("You are in!");
+        Terminal.WriteLine("Type menu to try again");
     }
 
     void RunMainMenu(string input)
     {
+        Terminal.ClearScreen();
         if (input == "1")
         {
             currentScreen = Screen.Password;
-            level = 1;
+            currentLevel = Level.Programming;
+            password = programmingPasswords[GenerateRandomNumber()];
+            Terminal.WriteLine(password);
             StartGame();
         }
         else if (input == "2")
         {
             currentScreen = Screen.Password;
-            level = 2;
+            currentLevel = Level.Hockey;
+            password = hockeyPasswords[GenerateRandomNumber()];
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            currentScreen = Screen.Password;
+            currentLevel = Level.Space;
+            password = spacePasswords[GenerateRandomNumber()];
             StartGame();
         }
         else
@@ -61,8 +104,8 @@ public class Hacker : MonoBehaviour
 
     void StartGame()
     {
-        Terminal.WriteLine("You have chosen level: " + level);
-        print("You have chosen level: " + level);
+        Terminal.WriteLine("What is the password? ");
+        Terminal.WriteLine(password.Anagram());
     }
 
     private void ShowMainMenu()
@@ -77,8 +120,9 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("3: C:\\Space");
     }
 
-    private void ChooseDirectory()
+    private int GenerateRandomNumber()
     {
-
+        System.Random randomNumber = new System.Random();
+        return randomNumber.Next(0, 5);
     }
 }
